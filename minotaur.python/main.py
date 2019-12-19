1917601f93c64cd1c649c5c4d1915090607c4719
Mirandatz <>
2019-12-19 14:08:31 -0300

    Updated paths.py to use the new directory structure
    Added new datasets to main.py

--- minotaur.python/main.py
+++ minotaur.python/main.py
@@ -18,42 +18,62 @@ def _generate_dataset_settings(dataset_name: str, classification_type: str,
                                                       fitness_metrics=['fscore'],
                                                       fittest_selection='nsga2',
                                                       max_generations=200,
-                                                      population_size=160,
+                                                      population_size=80,
                                                       mutants_per_generation=40,
                                                       cfsbe_target_instance_coverage=cfsbe_value)
         yield minotaur_settings
 
 
-def run_yeast():
-    for minotaur_settings in _generate_dataset_settings(dataset_name='yeast', classification_type='multilabel',
-                                                        cfsbe_values=[2, 3, 5, 10, 25, 50, 75, 100, 150, 250]):
-        minotaur.run_minotaur(minotaur_settings)
-
-
-def run_emotions():
-    for minotaur_settings in _generate_dataset_settings(dataset_name='emotions', classification_type='multilabel',
-                                                        cfsbe_values=[2, 3, 5, 10, 25, 50, 75, 100, 150, 250]):
-        minotaur.run_minotaur(minotaur_settings)
-
-
 def run_iris():
     for minotaur_settings in _generate_dataset_settings(dataset_name='iris', classification_type='singlelabel',
-                                                        cfsbe_values=[2, 3, 5, 10, 25, 50, 75]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64]):
         minotaur.run_minotaur(minotaur_settings)
 
 
 def run_breast_cancer_wisconsin():
     for minotaur_settings in _generate_dataset_settings(dataset_name='breast-cancer-wisconsin',
                                                         classification_type='singlelabel',
-                                                        cfsbe_values=[2, 3, 5, 10, 25, 50, 75]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64]):
         minotaur.run_minotaur(minotaur_settings)
 
 
-def run_eurovoc():
-    for minotaur_settings in _generate_dataset_settings(dataset_name='EUR-Lex (eurovoc descriptors)',
-                                                        classification_type='multilabel',
-                                                        cfsbe_values=[5]):
+def run_wine():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='wine',
+                                                        classification_type='singlelabel',
+                                                        cfsbe_values=[2, 8, 16, 32, 64]):
         minotaur.run_minotaur(minotaur_settings)
+
+
+def run_madelon():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='madelon',
+                                                        classification_type='singlelabel',
+                                                        cfsbe_values=[2, 16, 64, 128, 256]):
+        minotaur.run_minotaur(minotaur_settings)
+
+
+def run_yeast():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='yeast', classification_type='multilabel',
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+        minotaur.run_minotaur(minotaur_settings)
+
+
+def run_emotions():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='emotions', classification_type='multilabel',
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+        minotaur.run_minotaur(minotaur_settings)
+
+
+def run_scene():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='scene', classification_type='multilabel',
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+        minotaur.run_minotaur(minotaur_settings)
+
+
+def run_CAL500():
+    for minotaur_settings in _generate_dataset_settings(dataset_name='CAL500', classification_type='multilabel',
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+        minotaur.run_minotaur(minotaur_settings)
+
 
 
 def main():
@@ -62,18 +82,19 @@ def main():
         print('Invalid number of command line argumments. Expected 1')
         return
 
+    known_datasets = {'iris': run_iris,
+                      'breast-cancer-wisconsin': run_breast_cancer_wisconsin,
+                      'wine': run_wine,
+                      'madelon': run_madelon,
+                      'yeast': run_yeast,
+                      'emotions': run_emotions,
+                      'scene': run_scene,
+                      'CAL500': run_CAL500}
+
     dataset_name = args[1]
 
-    if dataset_name == 'yeast':
-        run_yeast()
-    elif dataset_name == 'emotions':
-        run_emotions()
-    elif dataset_name == 'iris':
-        run_iris()
-    elif dataset_name == 'breast-cancer-wisconsin':
-        run_breast_cancer_wisconsin()
-    elif dataset_name == 'eurovoc':
-        run_eurovoc()
+    if dataset_name in known_datasets:
+        known_datasets[dataset_name]()
     else:
         print("Unknown dataset")
 
--- minotaur.python/paths.py
+++ minotaur.python/paths.py
@@ -4,18 +4,18 @@ from typing import Dict
 
 _PC_NAME = platform.node()
 
-if _PC_NAME == 'TCHON':
+if _PC_NAME == 'Tchon':
     _base_dir = Path('c:/') / 'source'
     _DATASETS_BASE_DIR = _base_dir / 'minotaur.datasets'
     _OUTPUT_BASE_DIR = _base_dir / 'minotaur.output'
-    _MINOTAUR_PATH = _base_dir / 'minotaur' / 'minotaur' / 'minotaur' / 'bin' / 'x64' / 'release' / 'netcoreapp3.0'
+    _MINOTAUR_PATH = _base_dir / 'minotaur' / 'minotaur' / 'minotaur' / 'bin' / 'x64' / 'release' / 'netcoreapp3.1'
     _MINOTAUR_PATH /= 'Minotaur.exe'
 elif _PC_NAME == "bioinfo02":
     _base_dir = Path.home() / 'minotaur'
     _DATASETS_BASE_DIR = _base_dir / 'minotaur.datasets'
     _OUTPUT_BASE_DIR = _base_dir / 'minotaur.output'
     _MINOTAUR_PATH = _base_dir / 'minotaur.src' / 'minotaur' / 'Minotaur' / 'Minotaur' / 'bin' / 'x64' / 'Release'
-    _MINOTAUR_PATH = _MINOTAUR_PATH / 'netcoreapp3.0' / 'Minotaur'
+    _MINOTAUR_PATH = _MINOTAUR_PATH / 'netcoreapp3.1' / 'Minotaur'
 else:
     raise Exception("Unknown computer / paths.")
 
@@ -25,11 +25,11 @@ def get_minotaur_path() -> Path:
 
 
 def get_datasets_paths(dataset_name: str, fold_nr: int) -> Dict[str, Path]:
-    dataset_dir = _DATASETS_BASE_DIR / dataset_name / '2-ready-for-minotaur'
-    return {'train_data': dataset_dir / f'fold-{fold_nr}' / 'train-data.csv',
-            'train_labels': dataset_dir / f'fold-{fold_nr}' / 'train-labels.csv',
-            'test_data': dataset_dir / f'fold-{fold_nr}' / 'test-data.csv',
-            'test_labels': dataset_dir / f'fold-{fold_nr}' / 'test-labels.csv'}
+    dataset_dir = _DATASETS_BASE_DIR / dataset_name / 'folds'
+    return {'train_data': dataset_dir / str(fold_nr) / 'train-data.csv',
+            'train_labels': dataset_dir / str(fold_nr) / 'train-labels.csv',
+            'test_data': dataset_dir / str(fold_nr) / 'test-data.csv',
+            'test_labels': dataset_dir / str(fold_nr) / 'test-labels.csv'}
 
 
 def get_minotaur_output_path(dataset_name: str, fold_nr: int, output_dir_name: str) -> Path:
