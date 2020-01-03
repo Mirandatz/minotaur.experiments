e7f459ea943875558cc63f633683381f180ba911
Mirandatz <>
2020-01-03 11:26:37 -0300

    Added more to hyperparameter list cfsbe_value

--- minotaur.python/main.py
+++ minotaur.python/main.py
@@ -26,7 +26,7 @@ def _generate_dataset_settings(dataset_name: str, classification_type: str,
 
 def run_iris():
     for minotaur_settings in _generate_dataset_settings(dataset_name='iris', classification_type='singlelabel',
-                                                        cfsbe_values=[2, 8, 16, 32, 64]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128]):
         minotaur.run_minotaur(minotaur_settings)
 
 
@@ -34,6 +34,7 @@ def run_breast_cancer_wisconsin():
     for minotaur_settings in _generate_dataset_settings(dataset_name='breast-cancer-wisconsin',
                                                         classification_type='singlelabel',
                                                         cfsbe_values=[2, 8, 16, 32, 64]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
         minotaur.run_minotaur(minotaur_settings)
 
 
@@ -47,25 +48,25 @@ def run_wine():
 def run_madelon():
     for minotaur_settings in _generate_dataset_settings(dataset_name='madelon',
                                                         classification_type='singlelabel',
-                                                        cfsbe_values=[2, 16, 64, 128, 256]):
+                                                        cfsbe_values=[2, 16, 64, 128, 256, 512, 1024, 2048]):
         minotaur.run_minotaur(minotaur_settings)
 
 
 def run_yeast():
     for minotaur_settings in _generate_dataset_settings(dataset_name='yeast', classification_type='multilabel',
-                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]):
         minotaur.run_minotaur(minotaur_settings)
 
 
 def run_emotions():
     for minotaur_settings in _generate_dataset_settings(dataset_name='emotions', classification_type='multilabel',
-                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256, 512]):
         minotaur.run_minotaur(minotaur_settings)
 
 
 def run_scene():
     for minotaur_settings in _generate_dataset_settings(dataset_name='scene', classification_type='multilabel',
-                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256]):
+                                                        cfsbe_values=[2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]):
         minotaur.run_minotaur(minotaur_settings)
 
 
